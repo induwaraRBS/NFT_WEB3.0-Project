1b907377ba9f31124809476e39caaa92577dc525
induwara <induwarakanchana567@gmail.com>
2023-11-13 17:24:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://contenthub-static.crypto.com/wp_media/2023/08/TOP-10-NFT-TOKENS-TO-KNOW-IN-2023-.jpg"
+      />
     </div>
   );
 }
